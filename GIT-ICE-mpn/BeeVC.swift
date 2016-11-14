ca355c34d855d2cc623513c692aefe5c193bae01
Duncan <s517806@mail.nwmissouri.edu>
2016-11-14 16:42:18 -0600

    Changed FirstViewController to EhVC, SecondViewController to BeeVC, added third view controller names SeaVC.
    Added SeaVC to the TabController.

---
+++ GIT-ICE-mpn/BeeVC.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GIT-ICE-mpn
+//
+//  Created by Dufva,Duncan E on 11/14/16.
+//  Copyright © 2016 Dufva,Duncan E. All rights reserved.
+//
+
+import UIKit
+
+class BeeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
